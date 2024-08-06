a40f73746db765a9a372f3d810dcf3c0a7afc5bd
youonmyown <1337onmyown@gmail.com>
2024-08-06 11:49:16 +0300

    add gridstate, update server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
@@ -9,18 +11,26 @@ const io = socketIo(server);
 app.use(express.static('public'));
 
 const gridSize = 100 * 100;
-let gridState = Array(gridSize).fill('#ffffff');
+const gridFilePath = path.join(__dirname, 'gridState.json');
+
+let gridState = [];
+if (fs.existsSync(gridFilePath)) {
+    const data = fs.readFileSync(gridFilePath);
+    gridState = JSON.parse(data);
+} else {
+    gridState = Array(gridSize).fill('#ffffff');
+}
 
 io.on('connection', (socket) => {
     console.log('A user connected');
 
-    // Отправляем текущее состояние сетки новому пользователю
     socket.emit('initialGrid', gridState);
 
-    // Обрабатываем изменение цвета ячейки
     socket.on('colorChange', (data) => {
         gridState[data.index] = data.color;
-        // Отправляем изменения всем пользователям
+
+        fs.writeFileSync(gridFilePath, JSON.stringify(gridState));
+
         io.emit('updateCell', data);
     });
 
